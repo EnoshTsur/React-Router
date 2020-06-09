f76d03e64142a8cc30afb24eda2f0848b8fa64f3
gilos <gil@sasover.com>
2020-06-09 21:17:42 +0300

    map

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,29 @@
 import React from 'react'
 
 export default function About() {
+
+    // name, setName -> input
+    // greeting, setGreeting -> h1
+
+    // input.onChange -> setName(...)
+    // useEffect ? name -> if contains 'avi' ? setGreeting 'hahah' : 'bababa'
+    // h1 { greeting } 
+    
+    const hahaha = "hahaha"
+    const bababa = "bababa"
+    const avi = "avi"
+
+    const [ name, setName, ] = React.useState('')
+    const [ greeting, setGreeting, ] = React.useState(hahaha)
+
+    React.useEffect(() => {
+        setGreeting(name.includes(avi) ? hahaha : bababa)
+    }, [name])
+
     return (
         <div>
-            About
+            <input onChange={e => setName(e.target.value)}  placeholder="Name" />
+            <h1>{greeting}</h1>
         </div>
     )
 }
--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,9 +1,34 @@
 import React from 'react'
 
 export default function Contacts() {
+
+    const friends = ['avi', 'liat', 'ester', 'hanoch']
+
     return (
-        <div>
-            Contacts
+        <div style={{
+            margin: '1rem 0',
+            textAlign: 'center'
+        }}>
+            <h1> Contacts: </h1>
+
+            {
+                friends.map((name, index, arr) => {
+                    console.log(index, arr)
+
+                    return  ( <div key={`${name}${index}`} style={{
+                                    margin: '1rem auto',
+                                    padding: '1rem',
+                                    backgroundColor: '#d9d9d9',
+                                    borderRadius: '7px',
+                                    boxShadow: '0 2px 3px grey',
+                                    maxWidth: '70%'
+                                }}>
+                                <h3>{`Name: ${name}`}</h3>
+                                <h3>{`ID: ${index}`}</h3>
+                            </div> )
+                })
+            }
+
         </div>
     )
 }
