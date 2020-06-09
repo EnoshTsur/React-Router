dc9594e547296abd43af575e8cfa1d9475cb03b5
gilos <gil@sasover.com>
2020-06-09 18:59:09 +0300

    btns

--- src/App.js
+++ src/App.js
@@ -8,21 +8,17 @@ function App() {
             title="Click Me" 
             type="success" 
             onClick={() => console.log('hi')}
-            key="btn1"
-      
         />
 
         <Button 
             title="Click Me"  
             onClick={() => console.log('hi')}
-            key="btn2"
         />
 
         <Button 
             disabled
             title="Click Me"  
             onClick={() => console.log('hi')}
-            key="btn3"
         />
    </div>
   );
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,36 +1,20 @@
 import React from 'react'
+import classes from './Button.module.css'
 
-function Button({ title, onClick, disabled=false, type, key, }) {
+function Button({ title, onClick, disabled=false, type,  }) {
 
-    
+    const { Btn, BtnSuccess, BtnDefault,} = classes;
+
     const success = 'success'
 
-    const style  = {
-        margin: '0.5rem',
-        fontFamily: 'Raleway, sans-serif',
-        padding: '0.5rem 1rem',
-        color: 'white',
-        backgroundColor: type === success ? '#04AA51' :  '#d9d9d9',
-        borderRadius: '7px',
-        border: type === success ? '2px solid rgb(54, 209, 126)' : '2px solid #ccc',
-        ':hover': {
-            backgroundColor: type === success ? 'rgb(18, 122, 67)' : '#b3b3b3',
-            cursor: 'pointer'
-        },
-        ':focus': {
-            outline: 'none'
-        },
-        ':active': {
-            backgroundColor: type === success ? 'rgb(21, 90, 53)' : '#979797'
-        }
-    }
-
+    const className = type === success ? 
+    `${Btn} ${BtnSuccess}`: `${Btn} ${BtnDefault}`
+    
     return (
         <button 
             onClick={onClick} 
-            disabled={disabled}    
-            style={style}
-            key={key}
+            disabled={disabled}  
+            className={className}  
         >
             {title}
         </button>
