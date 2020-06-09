7e5b1e1a2be49eff78683641d7604111724ff6df
gilos <gil@sasover.com>
2020-06-09 19:38:21 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import Navigation from './components/Navigation/Navigation'
 import { Router, Switch, Route, } from 'react-router-dom'
+import { createBrowserHistory, } from 'history'
+import Homepage from './components/Homepage/Homepage'
+import About from './components/About/About'
+import Contacts from './components/Contacts/Contacts'
+
 
 function App() {
+
+  const history = createBrowserHistory()
+
   return (
     <div> 
         <Navigation />
+        <Router history={history}>
+            <Switch>
+                <Route exact path="/" component={Homepage} />
+                <Route exact path="/homepage" component={Homepage} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/contacts" component={Contacts} />
+            </Switch>
+        </Router>
    </div>
   );
 }
